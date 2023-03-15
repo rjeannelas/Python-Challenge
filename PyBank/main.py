d5cd78288f7320aa36ee5647b983345ec4f64eaa
rjeannelas <119624099+rjeannelas@users.noreply.github.com>
2023-03-14 21:17:03 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,18 @@
+# import library
+import os
+import csv
 
+# join path
+budget_data = os.path.join("Resources", "budget_data.csv")
+
+# open and read csv
+with open(budget_data, newline="") as csvfile:
+    csvreader = csv.reader(csv.reader(csvfile, delimter=',')
+    csv_header = next(csvreader)
+    print(f"Header: {csv_header}")
+                           
+    " find net amount of profit and loss
+    P = []
+    months = []
+                           
+    # read each row of data after header
